7836f44d93d79509e6c220dce59af526cae9046a
fireghost1812 <fireghost1812@yandex.ru>
2017-02-18 15:15:55 +0300

    create request and test

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 var ansi = require('ansi');
 var cursor = ansi(process.stdout);
 cursor.beep();
+//
+var request = require('request');
 
+request.get('http://nodeguide.ru/').pipe(
+    request.put('https://nodejs.org/api/stream.html')
+);
+console.log(request);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 var Lazy = require('lazy');
 
 var lazy = new Lazy;
@@ -16,3 +47,24 @@ lazy
   .join(function (xs) {
     console.log(xs);
   });
+  //
+  var express = require('express');
+var app = express.createServer();
+
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.listen(3000);
+
+  // Promt example
+var prompt = require('prompt');
+
+prompt.get(['first_name', 'last_name'], function (err, result) {
+    console.info('Command-line input received:');
+    console.log('  first_name: ' + result.first_name);
+    console.error('  last_name: ' + result.last_name);
+});
+
+prompt.start();
+*/
